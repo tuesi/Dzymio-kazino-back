bfc92a9a76b6ae3bab79d743f04f08ecf7911aa1
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2022-12-01 12:46:54 +0200

    line nerf

--- Rooms/lineRoom.js
+++ Rooms/lineRoom.js
@@ -23,7 +23,7 @@ var itemProbability = [];
 
 var coeficients = [0, 1, 1.5, 2, 4, 10];
 
-var itemProbabilityFactors = [400, 200, 55, 25, 15, 5];
+var itemProbabilityFactors = [400, 200, 100, 50, 25, 5];
 
 function lineSockets(lineIo) {
     io = lineIo;
