1674ddba79fef9b62a758341e0ed24f84ad22148
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2023-02-23 18:42:17 +0200

    make reset lives two hours earlyer

--- services/resetGetLives.js
+++ services/resetGetLives.js
@@ -4,7 +4,7 @@ const cron = require('node-cron');
 
 module.exports = () => {
 
-    var job = cron.schedule('00 00 * * *', async function () {
+    var job = cron.schedule('00 22 * * *', async function () {
         await resetAbleToGetLives();
     }, null, true, 'Europe/Vilnius');
 
